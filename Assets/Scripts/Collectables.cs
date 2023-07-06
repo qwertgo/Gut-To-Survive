1a564eb4cb805cd0dcb2ac8e5491709e672ca929
Btegz <137159255+Btegz@users.noreply.github.com>
2023-07-06 13:15:08 +0200

    Änderung

---
+++ Assets/Scripts/Collectables.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectables : MonoBehaviour
+{
+
+    // Update is called once per frame
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Collectable"))
+        {
+        Destroy(collision.gameObject);
+    }}
+}
