714d0054bc02a69b3722d30f3d1688ba433a539b
Btegz <137159255+Btegz@users.noreply.github.com>
2023-07-06 14:39:59 +0200

    SavePoints

--- Assets/Scripts/Collectables.cs
+++ Assets/Scripts/Collectables.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class Collectables : MonoBehaviour
 {
-
+    
     // Update is called once per frame
     private void OnTriggerEnter2D(Collider2D collision)
-    {
+    {   
+        
+
+        //if(collision.gameObject.layer == LayerMask.GetMask("Collectable"))
         if(collision.gameObject.CompareTag("Collectable"))
-        {
+        {  
         Destroy(collision.gameObject);
     }}
 }
