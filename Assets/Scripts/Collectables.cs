3035c3cb9c29cd03055991ef33b6e15f7173a67c
Btegz <137159255+Btegz@users.noreply.github.com>
2023-07-06 14:51:03 +0200

    Collectables

--- Assets/Scripts/Collectables.cs
+++ Assets/Scripts/Collectables.cs
@@ -8,11 +8,10 @@ public class Collectables : MonoBehaviour
     // Update is called once per frame
     private void OnTriggerEnter2D(Collider2D collision)
     {   
-        
 
-        //if(collision.gameObject.layer == LayerMask.GetMask("Collectable"))
-        if(collision.gameObject.CompareTag("Collectable"))
+        if(collision.gameObject.layer == LayerMask.NameToLayer("Collectable"))
         {  
-        Destroy(collision.gameObject);
-    }}
+            Destroy(collision.gameObject);
+        }
+    }
 }
