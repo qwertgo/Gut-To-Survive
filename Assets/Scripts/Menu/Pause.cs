091e8331e37327ebe46da98e881a3e6ad1d432e0
Btegz <137159255+Btegz@users.noreply.github.com>
2023-07-17 11:17:06 +0200

    Pause

--- Assets/Scripts/Menu/Pause.cs
+++ Assets/Scripts/Menu/Pause.cs
@@ -5,22 +5,25 @@ using UnityEngine;
 
 public class Pause : MonoBehaviour
 {
-  public GameObject Game;
+
   public GameObject PauseMenu;
 
-
+public GameObject Game;
+public bool pause;
   
      void Update()
     {
       
       if(Input.GetKey(KeyCode.Escape)) 
       {
-        
-       
-        Game.SetActive(false);
         PauseMenu.SetActive(true);
-    }
-     
+        pause = true;
+
+       
+      }
+      while(pause == true)
+         if(Input.GetKey(KeyCode.Escape)) 
+        Game.SetActive(true);
     }
 }
 
