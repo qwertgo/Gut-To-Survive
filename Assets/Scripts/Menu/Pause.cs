ff5a0d5e334147a13e540524d603c4ed0251e6a8
Samuel Galonska <samuelGalonska@protonmail.com>
2023-07-18 11:55:51 +0200

    basic Lighting Final Leveldesign

--- Assets/Scripts/Menu/Pause.cs
+++ Assets/Scripts/Menu/Pause.cs
@@ -21,7 +21,7 @@ public class Pause : MonoBehaviour
       if(Input.GetKey(KeyCode.Escape) && pause != true) 
       {
         PauseMenu.SetActive(true);
-       _Game.Find("Player").GetComponent<GameObject>().time.timeScale = 0;
+       //_Game.Find("Player").GetComponent<GameObject>().time.timeScale = 0;
         pause = true;
 
     
