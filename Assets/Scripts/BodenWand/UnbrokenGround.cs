40ed9bd73e366880bd5d154f93d124b2793e1105
Btegz <137159255+Btegz@users.noreply.github.com>
2023-07-26 17:21:41 +0200

    26.07

--- Assets/Scripts/BodenWand/UnbrokenGround.cs
+++ Assets/Scripts/BodenWand/UnbrokenGround.cs
@@ -9,6 +9,14 @@ public class UnbrokenGround : MonoBehaviour
   [SerializeField] GameObject brokenGroundObj;
   [SerializeField] float timePlayerCanStandOnPlattform;
 
+  public Quaternion originRotation; 
+  public Vector3 originPosition; 
+  public float shake_decay = 0.002f;
+	public float shake_intensity = .3f;
+
+	private float temp_shake_intensity = 0;
+
+
   void Start()
   {
     GameEvents.Respawn.AddListener(Activate);
@@ -30,6 +38,9 @@ public class UnbrokenGround : MonoBehaviour
     if(coll.gameObject.CompareTag("Player"))
     {
       StartCoroutine(WaitToDestroyYourself());
+    
+      Debug.Log("test");
+
     }   
   }
 
@@ -61,4 +72,24 @@ public class UnbrokenGround : MonoBehaviour
 
   }
   
+ /*void Update (){
+		if (temp_shake_intensity > 0){
+			transform.position = originPosition + Random.insideUnitSphere * temp_shake_intensity;
+			transform.rotation = new Quaternion(
+				originRotation.x + Random.Range (-temp_shake_intensity,temp_shake_intensity) * .2f,
+				originRotation.y + Random.Range (-temp_shake_intensity,temp_shake_intensity) * .2f,
+				originRotation.z + Random.Range (-temp_shake_intensity,temp_shake_intensity) * .2f,
+				originRotation.w + Random.Range (-temp_shake_intensity,temp_shake_intensity) * .2f);
+			temp_shake_intensity -= shake_decay;
+		}
+	}
+	
+	public void Shake()
+  {
+    
+		originPosition = transform.position;
+		originRotation = transform.rotation;
+		temp_shake_intensity = shake_intensity;
+    
+  }*/
 }
