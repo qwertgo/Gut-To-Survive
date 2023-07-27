82d7a29eee27ce873262963659310cc436205239
Btegz <137159255+Btegz@users.noreply.github.com>
2023-07-27 12:10:37 +0200

    EndScreen

--- Assets/Scripts/BodenWand/UnbrokenGround.cs
+++ Assets/Scripts/BodenWand/UnbrokenGround.cs
@@ -9,12 +9,7 @@ public class UnbrokenGround : MonoBehaviour
   [SerializeField] GameObject brokenGroundObj;
   [SerializeField] float timePlayerCanStandOnPlattform;
 
-  public Quaternion originRotation; 
-  public Vector3 originPosition; 
-  public float shake_decay = 0.002f;
-	public float shake_intensity = .3f;
 
-	private float temp_shake_intensity = 0;
 
 
   void Start()
@@ -71,25 +66,5 @@ public class UnbrokenGround : MonoBehaviour
     }
 
   }
-  
- /*void Update (){
-		if (temp_shake_intensity > 0){
-			transform.position = originPosition + Random.insideUnitSphere * temp_shake_intensity;
-			transform.rotation = new Quaternion(
-				originRotation.x + Random.Range (-temp_shake_intensity,temp_shake_intensity) * .2f,
-				originRotation.y + Random.Range (-temp_shake_intensity,temp_shake_intensity) * .2f,
-				originRotation.z + Random.Range (-temp_shake_intensity,temp_shake_intensity) * .2f,
-				originRotation.w + Random.Range (-temp_shake_intensity,temp_shake_intensity) * .2f);
-			temp_shake_intensity -= shake_decay;
-		}
-	}
-	
-	public void Shake()
-  {
-    
-		originPosition = transform.position;
-		originRotation = transform.rotation;
-		temp_shake_intensity = shake_intensity;
-    
-  }*/
+
 }
--- Assets/Scripts/end.cs
+++ Assets/Scripts/end.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class end : MonoBehaviour
 {
 public GameObject Highscore;
 public bool HA = false;
+public GameObject EndScreen;
 
  
+
 
     // Start is called before the first frame update
    public void OnCollisionEnter2D(Collision2D coll)
@@ -15,9 +19,11 @@ public bool HA = false;
         coll.gameObject.CompareTag("Player");
         {
         HA = true;
-        Debug.Log(HA);
-        Highscore.SetActive(true);      
+        EndScreen.SetActive(true);
+        Highscore.SetActive(true);
+
         }
         
     }
 }
+
